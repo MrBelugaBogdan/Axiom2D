536e2d30a934b636c5b5649624f4e05de2eecb0d
MrBelugaBogdan <medvedovbogdan19@gmail.com>
2026-07-17 13:56:51 +0300

    Update Renderer.h

--- Renderer.h
+++ Renderer.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <SDL2/SDL.h>
 #include "TileMap.h"
+#include "Engine.h"
 
 class Renderer {
 private:
@@ -8,55 +9,33 @@ private:
     int tileHeight = 32;
 
 public:
-    // Перетворення координат сітки в ізометричні координати екрана
-    void mapToScreen(int col, int row, int& screenX, int& screenY) {
-        screenX = (col - row) * (tileWidth / 2);
-        screenY = (col + row) * (tileHeight / 2);
-    }
-
-    void render(SDL_Renderer* sdlRenderer, TileMap& map, int camX, int camY) {
+    void render(SDL_Renderer* sdlRenderer, TileMap& map, Engine& core, int camX, int camY) {
         for (int x = 0; x < TileMap::WIDTH; ++x) {
             for (int y = 0; y < TileMap::HEIGHT; ++y) {
-                int screenX, screenY;
-                mapToScreen(x, y, screenX, screenY);
+                int screenX = (x - y) * (tileWidth / 2) + camX;
+                int screenY = (x + y) * (tileHeight / 2) + camY;
 
-                // Додаємо зсув камери
-                int drawX = screenX + camX;
-                int drawY = screenY + camY;
-
-                // Вибираємо колір залежно від ID блоку
                 Tile tile = map.grid[x][y];
-                if (tile.id == 0) {
-                    SDL_SetRenderDrawColor(sdlRenderer, 30, 144, 255, 255); // Синя вода
-                } else if (tile.id == 1) {
-                    SDL_SetRenderDrawColor(sdlRenderer, 34, 139, 34, 255);  // Зелена трава
-                } else if (tile.id == 2) {
-                    SDL_SetRenderDrawColor(sdlRenderer, 128, 128, 128, 255); // Сіра дорога
+                SDL_Texture* currentTex = nullptr;
+
+                // Обираємо правильну текстуру двигуна
+                if (tile.id == 0) currentTex = core.texWater;
+                else if (tile.id == 1) currentTex = core.texGrass;
+                else if (tile.id == 2) currentTex = core.texRoad;
+
+                if (currentTex) {
+                    // Малюємо текстуру, вписану в ромбоподібні межі
+                    SDL_Rect destRect = { screenX, screenY, tileWidth, tileHeight };
+                    SDL_RenderCopy(sdlRenderer, currentTex, NULL, &destRect);
                 }
 
-                drawIsoFilledTile(sdlRenderer, drawX, drawY);
+                // Малюємо сітку поверх для краси
+                SDL_SetRenderDrawColor(sdlRenderer, 0, 0, 0, 50);
+                SDL_RenderDrawLine(sdlRenderer, screenX, screenY + tileHeight / 2, screenX + tileWidth / 2, screenY);
+                SDL_RenderDrawLine(sdlRenderer, screenX + tileWidth / 2, screenY, screenX + tileWidth, screenY + tileHeight / 2);
+                SDL_RenderDrawLine(sdlRenderer, screenX + tileWidth, screenY + tileHeight / 2, screenX + tileWidth / 2, screenY + tileHeight);
+                SDL_RenderDrawLine(sdlRenderer, screenX + tileWidth / 2, screenY + tileHeight, screenX, screenY + tileHeight / 2);
             }
         }
-    }
-
-private:
-    // Малюємо зафарбований ізометричний ромб
-    void drawIsoFilledTile(SDL_Renderer* r, int x, int y) {
-        // Малюємо кілька ліній по вертикалі, щоб заповнити ромб кольором
-        for (int i = 0; i <= tileHeight / 2; ++i) {
-            int span = i * 2;
-            SDL_RenderDrawLine(r, x + tileWidth / 2 - span, y + i, x + tileWidth / 2 + span, y + i);
-        }
-        for (int i = 0; i <= tileHeight / 2; ++i) {
-            int span = (tileHeight / 2 - i) * 2;
-            SDL_RenderDrawLine(r, x + tileWidth / 2 - span, y + tileHeight / 2 + i, x + tileWidth / 2 + span, y + tileHeight / 2 + i);
-        }
-
-        // Малюємо чорну рамку навколо блоку для чіткості сітки
-        SDL_SetRenderDrawColor(r, 0, 0, 0, 255);
-        SDL_RenderDrawLine(r, x, y + tileHeight / 2, x + tileWidth / 2, y);
-        SDL_RenderDrawLine(r, x + tileWidth / 2, y, x + tileWidth, y + tileHeight / 2);
-        SDL_RenderDrawLine(r, x + tileWidth, y + tileHeight / 2, x + tileWidth / 2, y + tileHeight);
-        SDL_RenderDrawLine(r, x + tileWidth / 2, y + tileHeight, x, y + tileHeight / 2);
     }
 };
